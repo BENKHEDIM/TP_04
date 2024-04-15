abca5601f0af5869593bc2c6609320dccba63ba7
Benkhedim <benkhedimabdesselem@gmail.com>
2024-04-15 12:31:02 -0700

    first

---
+++ src/main/java/org/example/Order.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Order {
+    private int orderId;
+    private String customerName;
+
+    public Order(int orderId, String customerName) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+}
